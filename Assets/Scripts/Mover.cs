eb232bacc06ba44faab8b39d10fd5413838d2eef
Yee <yee.gamedev@gmail.com>
2023-01-30 10:29:46 -0600

    Player can move forward and backwards

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Mover : MonoBehaviour
+{
+    Vector3 moveDirection;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        moveDirection = transform.forward * Input.GetAxisRaw("Vertical");
+
+
+        rb.MovePosition(transform.position + moveDirection);
+    }
+}
