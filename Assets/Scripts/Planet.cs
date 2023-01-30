0b755a0c19024e8a9bd4b939790c990f9fa87ad6
Yee <yee.gamedev@gmail.com>
2023-01-30 11:32:16 -0600

    Optimization Refactors and Fixed Jittering

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -1,24 +1,40 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
     [SerializeField] string unit = "Unit";
-    [SerializeField] float gravityForce = 9.18f;
+    [SerializeField] float gravityForce = -10f;
 
-    private void OnTriggerStay(Collider other)
+    Dictionary<Transform, Rigidbody> pulledUnits = new Dictionary<Transform, Rigidbody>();
+
+    private void OnTriggerEnter(Collider other) => CheckIfAddToPulled(other);
+    private void OnTriggerExit(Collider other) => pulledUnits.Remove(other.transform);
+    private void Update() => RotateAndPull();
+
+    private void CheckIfAddToPulled(Collider other)
     {
-        if (other.CompareTag(unit))
+        if (other.CompareTag(unit) && !pulledUnits.ContainsKey(other.transform))
         {
-            Vector3 gravityUp = (other.transform.position - transform.position).normalized;
-            Vector3 bodyUp = other.transform.up;
-
-            Rigidbody rb = other.GetComponent<Rigidbody>();
-            rb.AddForce(gravityUp * gravityForce);
-
-            Quaternion rotation = Quaternion.FromToRotation(bodyUp, gravityUp) * other.transform.rotation;
-            other.transform.rotation = Quaternion.Slerp(other.transform.rotation, rotation, 50 * Time.deltaTime);
+            pulledUnits.Add(other.transform, other.GetComponent<Rigidbody>());
         }
     }
+
+    private void RotateAndPull()
+    {
+        foreach (KeyValuePair<Transform, Rigidbody> unit in pulledUnits)
+        {
+            Vector3 gravityDirection = (unit.Key.position - transform.position).normalized;
+            Rotate(unit.Key, gravityDirection);
+            Pull(unit.Value, gravityDirection);
+        }
+    }
+
+    private void Rotate(Transform unit, Vector3 gravityDirection)
+    {
+        Quaternion rotation = Quaternion.FromToRotation(unit.up, gravityDirection) * unit.rotation;
+        unit.rotation = Quaternion.FromToRotation(unit.up, gravityDirection) * unit.rotation;
+    }
+
+    private void Pull(Rigidbody unit, Vector3 gravityDirection) => unit.AddForce(gravityDirection * gravityForce);
 }
