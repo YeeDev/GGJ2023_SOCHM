ed5862da638406dc60322bfee6688e8369e81c4e
Yee <yee.gamedev@gmail.com>
2023-01-31 10:55:51 -0600

    Enemy can move around planet and follow targets

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,11 +5,57 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] string flameTag = "Flame";
+    [SerializeField] string enemyTag = "Player";
     [SerializeField] float damageTick = 0.1f;
     [SerializeField] int damageTaken = 1;
     [SerializeField] int health = 100;
+    [SerializeField] float moveSpeed = 2f;
 
     float damageTimer;
+    Rigidbody rb;
+    [SerializeField] Transform target;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    [SerializeField] float rayLength;
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            Vector3 direction = target.position - transform.position;
+            Vector3 step = direction;
+
+            Ray rayWilliamJohnson = new Ray(transform.position, direction);
+            Physics.Raycast(rayWilliamJohnson, out RaycastHit hit, rayLength);
+
+            if (hit.transform != target)
+            {
+                Vector3 projectedVector = Vector3.ProjectOnPlane(direction, hit.normal);
+                step = projectedVector.normalized * Time.deltaTime * moveSpeed;
+            }
+            else
+            {
+                step = direction.normalized * Time.deltaTime * moveSpeed; 
+            }
+
+            rb.MovePosition(transform.position + step);
+            transform.LookAt(target, transform.up);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag(enemyTag) && target == null) { target = other.transform; }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform == target) { target = null; }
+    }
 
     private void OnParticleCollision(GameObject other)
     {
