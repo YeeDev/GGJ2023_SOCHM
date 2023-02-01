91da90c6c81036cf2b8fb24415856acbdcdcad89
Yee <yee.gamedev@gmail.com>
2023-01-31 20:02:38 -0600

    Camera now moves to follow target

---
+++ Assets/Scripts/CamMover.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CamMover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    private void LateUpdate()
+    {
+        transform.position = target.position;
+        transform.rotation = target.rotation;
+    }
+}
