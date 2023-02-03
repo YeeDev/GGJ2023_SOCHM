cbf0278a97012f7b736d2aa89c443d85787072b3
Yee <yee.gamedev@gmail.com>
2023-02-02 20:33:28 -0600

    Roots! Roots Everywhere!

--- Assets/Scripts/EnemyAI_scr/RootStats.cs
+++ Assets/Scripts/EnemyAI_scr/RootStats.cs
@@ -5,14 +5,22 @@ public class RootStats : EnemyStats
 {
     public event Action<Transform> onTakingDamage;
 
+    [SerializeField] string damageParameter = "TakingDamage"; 
+
+    Animator anm;
     MegaVeggie megaVeggie;
 
-    private void Awake() => megaVeggie = GetComponentInParent<MegaVeggie>();
+    private void Awake()
+    {
+        anm = GetComponentInChildren<Animator>();
+        megaVeggie = GetComponentInParent<MegaVeggie>();
+    }
 
     public override void TakeDamage(GameObject other)
     {
         if (Time.time < damageTimer) { return; }
 
+        anm.SetTrigger(damageParameter);
         damageTimer = Time.time + damageTick;
         health -= damageTaken;
         if (onTakingDamage != null) { onTakingDamage(other.transform.parent); }
