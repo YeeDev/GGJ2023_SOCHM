b4a3368329d1d66b64c14da74f735f1c1f702c5e
Yee <yee.gamedev@gmail.com>
2023-02-03 16:23:39 -0600

    PostProcessing and few animations added

---
+++ Assets/Scripts/EnemyAI_scr/MVeggieStats.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MVeggieStats : EnemyStats
+{
+    [SerializeField] string damageParameter = "Damage";
+
+    Animator anm;
+
+    private void Awake() => anm = GetComponent<Animator>();
+
+    public override void TakeDamage(GameObject other)
+    {
+        if (Time.time < damageTimer) { return; }
+
+        anm.SetTrigger(damageParameter);
+        damageTimer = Time.time + damageTick;
+        health -= damageTaken;
+
+        if (health <= 0) { Destroy(gameObject); }
+    }
+}
--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -1,20 +1,21 @@
 using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class Teleporter : MonoBehaviour
 {
-    [SerializeField] Transform enemyTiedTo;
+    [SerializeField] Transform[] enemiesTiedTo;
     [SerializeField] Renderer lightPost;
     [SerializeField] ParticleSystem activatedEffect;
 
     private void Awake() => StartCoroutine(ActivateTeleporter());
 
-    public bool IsActive { get => enemyTiedTo == null; }
+    public bool IsActive { get => enemiesTiedTo.All(t => t == null); }
     public Vector3 TeleportPoint { get => transform.GetChild(0).position; }
 
     private IEnumerator ActivateTeleporter()
     {
-        yield return new WaitUntil(() => enemyTiedTo == null);
+        yield return new WaitUntil(() => IsActive);
 
         lightPost.materials[1].SetColor("_EmissionColor", Color.green);
         activatedEffect.Play();
